1c3cf22989cbaf022221bdf73d57ea8dc67ba448
Mithun Rosinth <44864907+MithunRosinth@users.noreply.github.com>
2022-07-05 10:11:28 +0530

    dry run

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-  res.send('Hello! Closet Server demo! Dry Run\n');
+  res.send('Hello! Closet Server demo! Demonstration\n');
 });
 
 var server = app.listen(3000, function () {
